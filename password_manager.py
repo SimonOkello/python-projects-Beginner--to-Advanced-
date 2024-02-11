2dd03c801992c02f930a0f698c156653f3c8c57c
SimonOkello <simonokello93@gmail.com>
2024-02-11 12:56:00 +0300

    password manager

--- password_manager.py
+++ password_manager.py
@@ -1,69 +1,84 @@
-import base64
-import os
+import tkinter as tk
+from tkinter import messagebox
 from cryptography.fernet import Fernet
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
+def generate_key():
+    return Fernet.generate_key()
 
-def load_key():
-    file = open('key.key', 'rb')
-    key = file.read()
-    file.close()
-    return key
+def encrypt_password(key, password):
+    f = Fernet(key)
+    return f.encrypt(password.encode()).decode()
 
+def decrypt_password(key, encrypted_password):
+    f = Fernet(key)
+    return f.decrypt(encrypted_password.encode()).decode()
 
-master_password = input('What is your master password?: ')
+passwords = {}
 
-key = load_key()
-fer = Fernet(key)
+def add_password():
+    service = service_entry.get()
+    username = username_entry.get()
+    password = password_entry.get()
 
-'''
-def write_key():
-    salt = os.urandom(16)
-    kdf = PBKDF2HMAC(
-        algorithm=hashes.SHA256(),
-        length=32,
-        salt=salt,
-        iterations=480000,
-    )
-    key = base64.urlsafe_b64encode(kdf.derive(master_password.encode()))
-    with open('key.key', 'wb') as key_file:
-        key_file.write(key)
-
-
-write_key()
-'''
-
-
-def view():
-    with open('passwords.txt', 'r') as pwd_file:
-        for line in pwd_file.readlines():
-            data = line.rstrip()
-            user, password = data.split('|')
-            print('User:', user, '| Password:',
-                  fer.decrypt(password).decode())
-
-
-def add():
-    username = input('Account Name: ')
-    password = input('Account Password: ')
-
-    with open('passwords.txt', 'a') as pwd_file:
-        pwd_file.write(
-            f'{username}|{fer.encrypt(password.encode()).decode()}\n')
-
-
-while True:
-    mode = input(
-        'Would you like to add a new password or view existing ones(add/view),press q to quit: ').lower()
-
-    if mode == 'q':
-        break
-
-    if mode == 'view':
-        view()
-    elif mode == 'add':
-        add()
+    if service and username and password:
+        encrypted_password = encrypt_password(key, password)
+        passwords[service] = {'username': username, 'password': encrypted_password}
+        messagebox.showinfo("Success", "Password added successfully!")
     else:
-        print('Invalid choice')
-        continue
+        messagebox.showwarning("Error", "Please fill in all the fields.")
+
+def get_password():
+    service = service_entry.get()
+    if service in passwords:
+        encrypted_password = passwords[service]['password']
+        decrypted_password = decrypt_password(key, encrypted_password)
+        messagebox.showinfo("Password", f"Username: {passwords[service]['username']}\nPassword: {decrypted_password}")
+    else:
+        messagebox.showwarning("Error", "Password not found.")
+
+key = generate_key()
+
+instructions = '''To add password fill all the fields and press "Add Password"
+To view password, enter Account Name and press "Get Password"'''
+signature = "Developed by Sai Satwik Bikumandla"
+
+window = tk.Tk()
+window.title("Password Manager")
+window.configure(bg="orange")
+
+window.resizable(False, False)
+
+
+center_frame = tk.Frame(window, bg="#d3d3d3")
+center_frame.grid(row=0, column=0, padx=10, pady=10)
+
+instruction_label = tk.Label(center_frame, text=instructions, bg="#d3d3d3")
+instruction_label.grid(row=0, column=1, padx=10, pady=5)
+
+service_label = tk.Label(center_frame, text="Account:", bg="#d3d3d3")
+service_label.grid(row=1, column=0, padx=10, pady=5)
+service_entry = tk.Entry(center_frame)
+service_entry.grid(row=1, column=1, padx=10, pady=5)
+
+username_label = tk.Label(center_frame, text="Username:", bg="#d3d3d3")
+username_label.grid(row=2, column=0, padx=10, pady=5)
+username_entry = tk.Entry(center_frame)
+username_entry.grid(row=2, column=1, padx=10, pady=5)
+
+password_label = tk.Label(center_frame, text="Password:", bg="#d3d3d3")
+password_label.grid(row=3, column=0, padx=10, pady=5)
+password_entry = tk.Entry(center_frame, show="*")
+password_entry.grid(row=3, column=1, padx=10, pady=5)
+
+
+add_button = tk.Button(center_frame, text="Add Password", command=add_password, height=1, width=10)
+add_button.grid(row=5, column=4, padx=10, pady=5)
+
+get_button = tk.Button(center_frame, text="Get Password", command=get_password, height=1, width=10)
+get_button.grid(row=6, column=4, padx=10, pady=5)
+
+signature_label = tk.Label(center_frame, text=signature, bg="#d3d3d3")
+signature_label.grid(row=7, column=1, padx=5, pady=5)
+
+
+window.mainloop()
