ea7fa7256dbd09bbf3058bcbb8de623727779a3b
SimonOkello <simonokello93@gmail.com>
2024-02-12 10:40:54 +0300

    password manager

--- password_manager.py
+++ password_manager.py
@@ -1,84 +1,50 @@
-import tkinter as tk
-from tkinter import messagebox
 from cryptography.fernet import Fernet
 
-def generate_key():
-    return Fernet.generate_key()
+'''
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)'''
 
-def encrypt_password(key, password):
-    f = Fernet(key)
-    return f.encrypt(password.encode()).decode()
 
-def decrypt_password(key, encrypted_password):
-    f = Fernet(key)
-    return f.decrypt(encrypted_password.encode()).decode()
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
 
-passwords = {}
 
-def add_password():
-    service = service_entry.get()
-    username = username_entry.get()
-    password = password_entry.get()
+key = load_key()
+fer = Fernet(key)
 
-    if service and username and password:
-        encrypted_password = encrypt_password(key, password)
-        passwords[service] = {'username': username, 'password': encrypted_password}
-        messagebox.showinfo("Success", "Password added successfully!")
+
+def view():
+    with open('passwords.txt', 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, passw = data.split("|")
+            print("User:", user, "| Password:",
+                  fer.decrypt(passw.encode()).decode())
+
+
+def add():
+    name = input('Account Name: ')
+    pwd = input("Password: ")
+
+    with open('passwords.txt', 'a') as f:
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
+
+
+while True:
+    mode = input(
+        "Would you like to add a new password or view existing ones (view, add), press q to quit? ").lower()
+    if mode == "q":
+        break
+
+    if mode == "view":
+        view()
+    elif mode == "add":
+        add()
     else:
-        messagebox.showwarning("Error", "Please fill in all the fields.")
-
-def get_password():
-    service = service_entry.get()
-    if service in passwords:
-        encrypted_password = passwords[service]['password']
-        decrypted_password = decrypt_password(key, encrypted_password)
-        messagebox.showinfo("Password", f"Username: {passwords[service]['username']}\nPassword: {decrypted_password}")
-    else:
-        messagebox.showwarning("Error", "Password not found.")
-
-key = generate_key()
-
-instructions = '''To add password fill all the fields and press "Add Password"
-To view password, enter Account Name and press "Get Password"'''
-signature = "Developed by Sai Satwik Bikumandla"
-
-window = tk.Tk()
-window.title("Password Manager")
-window.configure(bg="orange")
-
-window.resizable(False, False)
-
-
-center_frame = tk.Frame(window, bg="#d3d3d3")
-center_frame.grid(row=0, column=0, padx=10, pady=10)
-
-instruction_label = tk.Label(center_frame, text=instructions, bg="#d3d3d3")
-instruction_label.grid(row=0, column=1, padx=10, pady=5)
-
-service_label = tk.Label(center_frame, text="Account:", bg="#d3d3d3")
-service_label.grid(row=1, column=0, padx=10, pady=5)
-service_entry = tk.Entry(center_frame)
-service_entry.grid(row=1, column=1, padx=10, pady=5)
-
-username_label = tk.Label(center_frame, text="Username:", bg="#d3d3d3")
-username_label.grid(row=2, column=0, padx=10, pady=5)
-username_entry = tk.Entry(center_frame)
-username_entry.grid(row=2, column=1, padx=10, pady=5)
-
-password_label = tk.Label(center_frame, text="Password:", bg="#d3d3d3")
-password_label.grid(row=3, column=0, padx=10, pady=5)
-password_entry = tk.Entry(center_frame, show="*")
-password_entry.grid(row=3, column=1, padx=10, pady=5)
-
-
-add_button = tk.Button(center_frame, text="Add Password", command=add_password, height=1, width=10)
-add_button.grid(row=5, column=4, padx=10, pady=5)
-
-get_button = tk.Button(center_frame, text="Get Password", command=get_password, height=1, width=10)
-get_button.grid(row=6, column=4, padx=10, pady=5)
-
-signature_label = tk.Label(center_frame, text=signature, bg="#d3d3d3")
-signature_label.grid(row=7, column=1, padx=5, pady=5)
-
-
-window.mainloop()
+        print("Invalid mode.")
+        continue
